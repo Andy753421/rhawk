b71c2b64c9f8a3d12d622b0b84b5946e8b1353f3
Andy Spencer <andy753421@gmail.com>
2013-04-06 04:38:48 +0000

    Add TCP keep alive options
    This should cause the connection to disconnect after a network outage.

--- rhawk.sh
+++ rhawk.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+KEEPALIVE="keepalive,keepidle=240,keepcnt=1,keepintvl=1"
 cd /home/andy/src/rhawk
 while true; do
-	socat TCP:'irc.freenode.net:6667' EXEC:'awk -f rhawk'
+	socat TCP:"irc.freenode.net:6667,$KEEPALIVE" EXEC:"awk -f rhawk"
 	sleep 30
 done
